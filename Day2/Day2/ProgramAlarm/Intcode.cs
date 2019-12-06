8b9f0b8893504b068ecc073040a821a3eb059293
Al Mills <al.mills@runpath.com>
2019-12-06 20:07:59 +0000

    part 1

--- Day2/Day2/ProgramAlarm/Intcode.cs
+++ Day2/Day2/ProgramAlarm/Intcode.cs
@@ -7,7 +7,29 @@ namespace ProgramAlarm
     {
         public static string Run(string input)
         {
-            int[] values = input.Split(new char[',']).Select(s => int.Parse(s)).ToArray();
+            int[] values = input.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+
+            for(int i = 0; i < values.Length; i += 4)
+            {
+                switch(values[i])
+                {
+                    case 1:
+                        var posToUpdate = values[i + 3];
+                        var a = values[i + 1];
+                        var b = values[i + 2];
+                        values[posToUpdate] = values[a] + values[b];
+                        break;
+                    case 2:
+                        posToUpdate = values[i + 3];
+                        a = values[i + 1];
+                        b = values[i + 2];
+                        values[posToUpdate] = values[a] * values[b];
+                        break;
+                    case 99:
+                        string.Join(',', values);
+                        break;
+                }   
+            }
 
             return string.Join(',', values);
         }
